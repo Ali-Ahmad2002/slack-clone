b9896dfd17737d033c607d5953635785e36c9ca1
Ali-Ahmad2002 <aliahmad.557755@gmail.com>
2022-04-03 02:57:30 +0200

    threads

--- src/app/threads/threads.component.ts
+++ src/app/threads/threads.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { DataService } from '../data.service';
 import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Message } from 'src/models/message';
 
 @Component({
   selector: 'app-threads',
@@ -12,22 +13,22 @@ export class ThreadsComponent implements OnInit {
 
   chatId!: any;
   message!: string;
+  answer!: Message[];
+  threadMsg: any = [];
 
   constructor(
     public firestore: AngularFirestore,
     public data: DataService,
     public route: ActivatedRoute
   ) { }
-
+  
   ngOnInit(): void {
     // this.route.paramMap.subscribe(paramMap => {
     //   this.chatId = paramMap.get('id');
     //   console.log(this.chatId)
     // });
-    setTimeout(() => {
-      console.log(this.data);
-      console.log(this.data.clickedChat);
-    }, 3000);
+    console.log(this.data);
+    console.log(this.data.clickedChat);
   }
 
   closeThread() {
@@ -35,8 +36,11 @@ export class ThreadsComponent implements OnInit {
   }
 
   showThreadMessage() {
-    console.log(this.message);
+    // console.log(this.message);
+    const newThreadMsg = new Message();
+    newThreadMsg.text = this.message;
     this.message = '';
+    console.log('MSG', newThreadMsg)
   }
 
 }
