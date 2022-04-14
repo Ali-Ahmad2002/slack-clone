0a6b5c094807b5e63510945b55682f77c9d9a73a
Ali-Ahmad2002 <aliahmad.557755@gmail.com>
2022-04-14 19:51:21 +0200

    dmChat
    Co-authored-by: Cagri Avsar <CagriAvsar@users.noreply.github.com>

--- src/app/dms/dms.component.ts
+++ src/app/dms/dms.component.ts
@@ -12,14 +12,15 @@ import { AuthService } from '../shared/services/auth.service';
 })
 export class DmsComponent implements OnInit {
 
-  
-  test:any;
+
+  test: any;
   user!: User[];
-  userTest!:any;
-  dmsMessage!:any;
-  
-  
-  constructor(  
+  userTest!: any;
+  dmsMessage!: any;
+
+
+
+  constructor(
     public data: DataService,
     public router: Router,
     public firestore: AngularFirestore,
@@ -29,52 +30,63 @@ export class DmsComponent implements OnInit {
 
 
     this.firestore
-    .collection('users')
-    .valueChanges()
-    .subscribe((users: any) => {
-   this.test = users
-      console.log('chats', this.test);
-      
-    })
+      .collection('users')
+      .valueChanges()
+      .subscribe((users: any) => {
+        this.test = users
+        console.log('chats', this.test);
+
+      })
 
   }
 
 
-  showDms(us:any){
+  showDms(us: any) {
     console.log('Halo DMS', us);
 
     this.userTest = us;
     this.firestore
-    .collection('users', ref => ref.where('chatId', '==', this.authService.userData.multiFactor.user.uid))
-    .valueChanges({ idField: 'id' })
-    .subscribe((users: any) => {
-      this.user = users.map((messa: any) => new User(messa))
-      console.log('user',this.user)
+      .collection('messages', ref => ref.where('chatId', '==', this.userTest.uid))
+      .valueChanges({ idField: 'id' })
+      .subscribe((users: any) => {
+       // this.user = users.map((users: any) => new User(users))
+        console.log('user',users)
+      });
 
+    // this.userTest = us;
+    // this.firestore
+    //   .collection('dms')
+    //   .valueChanges({ idField: 'id' })
+    //   .subscribe((us: any) => {
+        //  this.myDMS = us.filter( m => m.users.contains(MEINE_USER_ID))
+        //this.user = users.map((users: any) => new User(users))
+      //   console.log('user2', us)
+      //   this.test = us
 
-    });
+      // });
 
 
 
     const newThreadMsg = new User();
     console.log('neetreas', newThreadMsg);
-    
-   // newThreadMsg.text = this.dmsMessage;
-   newThreadMsg.displayName = this.authService.userData.multiFactor.user.email;
+
+    // newThreadMsg.text = this.dmsMessage;
+    newThreadMsg.displayName = this.authService.userData.multiFactor.user.email;
     newThreadMsg.id = this.authService.userData.multiFactor.user.uid;
     newThreadMsg.uid = this.userTest.uid;
     newThreadMsg.email = this.userTest.email;
-    
+    newThreadMsg.text =  'Halo ALi'
+
 
     this.firestore.collection('dms')
       .add(newThreadMsg.toJson())
-      console.log('ANSWER', newThreadMsg);
-    
+    console.log('ANSWER', newThreadMsg);
+
   }
 
 
 
 
- 
+
 
 }
